95c18142caf3838a23b626b30a73e6ee9dbebcc8
Leonardo de Almeida Santos <leonardo.almeida@rivendel.com.br>
2017-02-19 17:54:35 -0300

    deploy to heroku

--- app/core/dao/connection.py
+++ app/core/dao/connection.py
@@ -1,24 +1,39 @@
 import psycopg2
 import os
+from urllib.parse import urlparse
 
 class Connection(object):
     def __init__(self):
-        self.host = os.environ['DB_HOST']
-        self.user = os.environ['DB_USER']
-        self.password = os.environ['DB_PASS']
-        self.dbname = os.environ['DB_NAME']
+        # self.host = os.environ['DB_HOST']
+        # self.user = os.environ['DB_USER']
+        # self.password = os.environ['DB_PASS']
+        # self.dbname = os.environ['DB_NAME']
+        # urlparse.uses_netloc.append('postgres')
+        self.url = urlparse(os.environ['DATABASE_URL'])
 
     def connect(self):
         try:
-            conn = psycopg2.connect(host=self.host,
-                    user=self.user,
-                    password=self.password,
-                    dbname=self.dbname
-                )
+            conn = psycopg2.connect(
+                database=self.url.path[1:],
+                user=self.url.username,
+                password=self.url.password,
+                host=self.url.hostname,
+                port=self.url.port
+            )
             print('connected with postgresql database')
             return conn
         except:
-            raise ConnectionError('not able to connection with database')
+            raise ConnectionError('not able to connect with database')
+        # try:
+        #     conn = psycopg2.connect(host=self.host,
+        #             user=self.user,
+        #             password=self.password,
+        #             dbname=self.dbname
+        #         )
+        #     print('connected with postgresql database')
+        #     return conn
+        # except:
+        #     raise ConnectionError('not able to connection with database')
 
 
 class ConnectionError(Exception):
