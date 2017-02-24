1efed71753d3ef0b90a9508d48c782b63602d468
Leonardo de Almeida Santos <leonardo.almeida@rivendel.com.br>
2017-02-24 10:15:53 -0300

    update repo with att in heroku

--- app/core/dao/connection.py
+++ app/core/dao/connection.py
@@ -4,11 +4,6 @@ from urllib.parse import urlparse
 
 class Connection(object):
     def __init__(self):
-        # self.host = os.environ['DB_HOST']
-        # self.user = os.environ['DB_USER']
-        # self.password = os.environ['DB_PASS']
-        # self.dbname = os.environ['DB_NAME']
-        # urlparse.uses_netloc.append('postgres')
         self.url = urlparse(os.environ['DATABASE_URL'])
 
     def connect(self):
@@ -24,17 +19,6 @@ class Connection(object):
             return conn
         except:
             raise ConnectionError('not able to connect with database')
-        # try:
-        #     conn = psycopg2.connect(host=self.host,
-        #             user=self.user,
-        #             password=self.password,
-        #             dbname=self.dbname
-        #         )
-        #     print('connected with postgresql database')
-        #     return conn
-        # except:
-        #     raise ConnectionError('not able to connection with database')
-
 
 class ConnectionError(Exception):
     def __init__(self, message):
