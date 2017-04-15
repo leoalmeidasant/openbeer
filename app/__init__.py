e9083e7a03e8a5017ed9ab0d1416cb2de48dded0
Leonardo De Almeida Santos <leonardodealmeidasantos@MBP-de-Leonardo.home>
2017-04-14 21:12:11 -0300

    add config to sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+import logging
+
+logging.basicConfig()
+logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
+db = SQLAlchemy(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
--- app/config.py
+++ app/config.py
@@ -1,1 +1,5 @@
+import os
+
 SECRET_KEY='secretkeytoforms'
+
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
